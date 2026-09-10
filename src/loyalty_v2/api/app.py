8298979078b7be175671fec52bfefbcf704209d0
Tagir <stomvoshod@gmail.com>
2026-09-11 00:40:34 +0300

    Mount engagement API routes

--- src/loyalty_v2/api/app.py
+++ src/loyalty_v2/api/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from loyalty_v2.api.admin_config_routes import router as admin_config_router
 from loyalty_v2.api.admin_customer_routes import router as admin_customer_router
 from loyalty_v2.api.admin_staff_routes import router as admin_staff_router
+from loyalty_v2.api.engagement_routes import router as engagement_router
 from loyalty_v2.api.secure_policy_routes import router as secure_policy_router
 from loyalty_v2.api.secure_routes import router as secure_router
 from loyalty_v2.core.config import get_settings
@@ -14,6 +15,7 @@ app.include_router(secure_policy_router)
 app.include_router(admin_config_router)
 app.include_router(admin_staff_router)
 app.include_router(admin_customer_router)
+app.include_router(engagement_router)
 
 
 @app.get("/health", tags=["system"])
