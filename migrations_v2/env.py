b24c871a20605612e68223dc035d46a53e41dc6e
Tagir <stomvoshod@gmail.com>
2026-09-11 00:40:23 +0300

    Load engagement models in Alembic metadata

--- migrations_v2/env.py
+++ migrations_v2/env.py
@@ -13,6 +13,7 @@ from loyalty_v2.db import audit_models  # noqa: F401
 from loyalty_v2.db import auth_models  # noqa: F401
 from loyalty_v2.db import category_models  # noqa: F401
 from loyalty_v2.db import customer_policy_models  # noqa: F401
+from loyalty_v2.db import engagement_models  # noqa: F401
 from loyalty_v2.db import identity_models  # noqa: F401
 from loyalty_v2.db import milestone_models  # noqa: F401
 from loyalty_v2.db import models  # noqa: F401
