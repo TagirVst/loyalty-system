724f9cc14da4bb1295baccad9b852618967aed99
Tagir <stomvoshod@gmail.com>
2026-09-11 00:45:48 +0300

    Mount authenticated customer API

--- src/loyalty_v2/api/app.py
+++ src/loyalty_v2/api/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from loyalty_v2.api.admin_config_routes import router as admin_config_router
 from loyalty_v2.api.admin_customer_routes import router as admin_customer_router
 from loyalty_v2.api.admin_staff_routes import router as admin_staff_router
+from loyalty_v2.api.customer_routes import router as customer_router
 from loyalty_v2.api.engagement_routes import router as engagement_router
 from loyalty_v2.api.secure_policy_routes import router as secure_policy_router
 from loyalty_v2.api.secure_routes import router as secure_router
@@ -16,6 +17,7 @@ app.include_router(admin_config_router)
 app.include_router(admin_staff_router)
 app.include_router(admin_customer_router)
 app.include_router(engagement_router)
+app.include_router(customer_router)
 
 
 @app.get("/health", tags=["system"])
