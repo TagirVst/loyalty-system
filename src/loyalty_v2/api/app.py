e07cef510baa92f079430032044874353564ccc6
Tagir <stomvoshod@gmail.com>
2026-09-11 09:54:52 +0300

    Register admin override routes

--- src/loyalty_v2/api/app.py
+++ src/loyalty_v2/api/app.py
@@ -4,6 +4,7 @@ from loyalty_v2.api.admin_analytics_routes import router as admin_analytics_rout
 from loyalty_v2.api.admin_config_routes import router as admin_config_router
 from loyalty_v2.api.admin_customer_routes import router as admin_customer_router
 from loyalty_v2.api.admin_notification_routes import router as admin_notification_router
+from loyalty_v2.api.admin_override_routes import router as admin_override_router
 from loyalty_v2.api.admin_staff_routes import router as admin_staff_router
 from loyalty_v2.api.customer_routes import router as customer_router
 from loyalty_v2.api.engagement_routes import router as engagement_router
@@ -19,6 +20,7 @@ app.include_router(secure_policy_router)
 app.include_router(admin_config_router)
 app.include_router(admin_staff_router)
 app.include_router(admin_customer_router)
+app.include_router(admin_override_router)
 app.include_router(admin_notification_router)
 app.include_router(admin_analytics_router)
 app.include_router(integration_router)
